8b7e2d0be5aa531c789a0796f798a1452326035d
Adam Pierzchała <pierzchala.adam@gmail.com>
2013-09-23 10:44:15 +0200

    poprawione czytanie urla

--- script/confirm.js
+++ script/confirm.js
@@ -1,29 +1,5 @@
-var QueryString = function () {
-    // This function is anonymous, is executed immediately and
-    // the return value is assigned to QueryString!
-    var query_string = {};
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i<vars.length;i++) {
-        var pair = vars[i].split("=");
-        // If first entry with this name
-        if (typeof query_string[pair[0]] === "undefined") {
-            query_string[pair[0]] = pair[1];
-            // If second entry with this name
-        } else if (typeof query_string[pair[0]] === "string") {
-            var arr = [ query_string[pair[0]], pair[1] ];
-            query_string[pair[0]] = arr;
-            // If third or later entry with this name
-        } else {
-            query_string[pair[0]].push(pair[1]);
-        }
-    }
-    return query_string;
-} ();
-
-
 $(document).ready(function () {
-    $.ajax("http://46.105.25.37:8181/confirm/" + QueryString.v)
+    $.ajax("http://46.105.25.37:8181/confirm/" + window.location.search.substring(3))
         .success(function() {
             $("h4").text("REzErwacja potwIErdzona!");
         })
